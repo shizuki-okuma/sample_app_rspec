b25176dd8bccc1379af7299fa5b611333a05d083
shizuki-okuma <126657775+shizuki-okuma@users.noreply.github.com>
2023-06-11 01:53:10 +0000

    [edit]routes application.thml

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-   get 'top' => 'homes#top'
-  resources :lists
+  get 'homes/top' => 'homes#top'
+  get 'lists/new'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
